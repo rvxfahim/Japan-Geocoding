05fbfab1405f7e36779e12b0df8b2cd6693eeed2
Tahmid Zahin <tzahin10@gmail.com>
2022-02-17 02:54:56 +0600

    take japanese string input from keyboard

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ jageocoder.init()
 search_string='新宿区西新宿２－８－１'
 result=json.dumps((jageocoder.search(search_string)), ensure_ascii=False).encode('utf-8')
 print(result.decode())
+# take keyboard input
+input_string=input('Enter a japanese string: ')
+result=json.dumps((jageocoder.search(input_string)), ensure_ascii=False).encode('utf-8')
+print(result.decode())
